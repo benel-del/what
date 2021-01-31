261d914127984491023e66165523e7f2a04d9dd0
apphia39 <67676029+apphia39@users.noreply.github.com>
2021-01-31 23:06:18 +0900

    Bbs_faq.java

--- src/DB/Bbs_faq.java
+++ src/DB/Bbs_faq.java
@@ -1,5 +1,48 @@
 package DB;
 
 public class Bbs_faq extends Bbs{
-
+	private int bbsID;
+	private String bbsTitle;
+	private String writer;
+	private String bbsDate;
+	private String bbsContent;
+	private int bbsAvailable;
+	public int getBbsID() {
+		return bbsID;
+	}
+	public void setBbsID(int bbsID) {
+		this.bbsID = bbsID;
+	}
+	public String getBbsTitle() {
+		return bbsTitle;
+	}
+	public void setBbsTitle(String bbsTitle) {
+		this.bbsTitle = bbsTitle;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getBbsDate() {
+		return bbsDate;
+	}
+	public void setBbsDate(String bbsDate) {
+		this.bbsDate = bbsDate;
+	}
+	public String getBbsContent() {
+		return bbsContent;
+	}
+	public void setBbsContent(String bbsContent) {
+		this.bbsContent = bbsContent;
+	}
+	public int getBbsAvailable() {
+		return bbsAvailable;
+	}
+	public void setBbsAvailable(int bbsAvailable) {
+		this.bbsAvailable = bbsAvailable;
+	}
+	
+	
 }
